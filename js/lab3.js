ff70250465d705eca81367d6bcf5a5da117a9851
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-25 19:56:11 -0400

    style CSS for active tab

--- js/lab3.js
+++ js/lab3.js
@@ -13,10 +13,10 @@ let tabItem = document.querySelectorAll('.tabs ul li a');
 
 tabItem.forEach(function(tab) {
     tab.addEventListener('click', function() {
-        // Remove active class from all tabs
-        document.querySelectorAll('.tabs ul li').forEach(function(tabItem) {
-            tabItem.classList.remove('active');
-        });
+        // // Remove active class from all tabs
+        // document.querySelectorAll('.tabs ul li').forEach(function(tabItem) {
+        //     tabItem.classList.remove('active');
+        // });
         // Add active class to the clicked tab
         tab.parentNode.classList.add('active');
         // Hide all tab content panels
