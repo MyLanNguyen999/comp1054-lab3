ec3decf9d8ffcd6716d7c13cc7e72ff3f74b0cf4
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-25 19:52:10 -0400

    added JS for active item

--- js/lab3.js
+++ js/lab3.js
@@ -1,10 +1,29 @@
-const tabPanels = document.querySelectorAll(".tabpanel");
+// const tabPanels = document.querySelectorAll(".tabpanel");
 
-tabPanels.forEach((tabPanel) => {
-	tabPanel.addEventListener("click", closeOpenDetails)
+// tabPanels.forEach((tabPanel) => {
+// 	tabPanel.addEventListener("click", closeOpenDetails)
+// });
+// let tabId1 = document.getElementById("tabpanel1");
+// console.log(tabId1);
+// let tabId2 = document.getElementById("tabpanel2");
+// let tabId3 = document.getElementById("tabpanel3");
+
+
+let tabItem = document.querySelectorAll('.tabs ul li a');
+
+tabItem.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+        // Remove active class from all tabs
+        document.querySelectorAll('.tabs ul li').forEach(function(tabItem) {
+            tabItem.classList.remove('active');
+        });
+        // Add active class to the clicked tab
+        tab.parentNode.classList.add('active');
+        // Hide all tab content panels
+        document.querySelectorAll('.tabpanel').forEach(function(panel) {
+            panel.classList.remove('active');
+        });
+        // Show the corresponding tab content panel
+        document.querySelector(this.getAttribute('href')).classList.add('active');
+    });
 });
-
-function closeOpenDetails() {
-    tabPanels.forEach((tabPanel) => {
-        let tabClick = 
-}
